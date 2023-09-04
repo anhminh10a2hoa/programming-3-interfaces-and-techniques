032db9623aa4bd8a580df073fbd5b6f16f7a8163
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-09-05 00:38:30 +0300

    3.3: Parameter table

---
+++ round3/parameters/src/main/java/Parameters.java
@@ -0,0 +1,55 @@
+package fi.tuni.prog3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Parameters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<String> parameters = new ArrayList<>();
+
+        System.out.println("Parameters:");
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (input.isEmpty()) {
+                break;
+            }
+            parameters.add(input);
+        }
+
+        // Sort the parameters alphabetically
+        Collections.sort(parameters);
+
+        // Calculate column widths
+        int col1Width = String.valueOf(parameters.size()).length() + 2; // Add 2 for padding
+        int col2Width = parameters.stream().mapToInt(String::length).max().orElse(0) + 2; // Add 2 for padding
+
+        createBorder(col1Width, col2Width, "a");
+
+        // Print the table content
+        for (int i = 0; i < parameters.size(); i++) {
+            System.out.format("# %-" + (col1Width - 2) + "d | %-" + (col2Width - 2) + "s #\n", i + 1, parameters.get(i));
+            if(i != parameters.size()) {
+                createBorder(col1Width, col2Width, "middle");
+            }
+        }
+
+        // Print the table footer
+        createBorder(col1Width, col2Width, "a");
+    }
+
+    private static void createBorder(int col1Width, int col2Width, String type) {
+        System.out.print("#");
+        for (int i = 0; i < col1Width; i++) {
+            System.out.print(type == "middle" ? "-" : "#");
+        }
+        System.out.print(type == "middle" ? "+" : "#");
+        for (int i = 0; i < col2Width; i++) {
+            System.out.print(type == "middle" ? "-" : "#");
+        }
+        System.out.println("#");
+    }
+}
