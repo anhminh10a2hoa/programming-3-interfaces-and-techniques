0d60eac77426f117ec2cac22ffdca94f06ab2cac
Minh Hoang <anhminh10a2hoa@gmail.com>
2023-10-24 05:02:50 +0300

    fixing package-info.java

--- round10/javadocjson/src/main/java/fi/tuni/prog3/json/package-info.java
+++ round10/javadocjson/src/main/java/fi/tuni/prog3/json/package-info.java
@@ -1,12 +1,1 @@
-/**
- * Provides tools for representing data in JSON format.
- * <p>
- * This package contains classes for working with JSON data, including JSON objects, arrays, and values.
- * You can use these classes to create, manipulate, and interact with JSON data.
- * </p>
- *
- * @see fi.tuni.prog3.json.ArrayNode
- * @see fi.tuni.prog3.json.ObjectNode
- * @see fi.tuni.prog3.json.ValueNode
- */
 package fi.tuni.prog3.json;
